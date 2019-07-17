cbc490f18ba39e9a3ef86543724097cc471bd81e
Dilek Ozkan <50422693+dilekozkan@users.noreply.github.com>
2019-07-17 13:49:22 +0300

    Update AppCode.py

---
+++ AppCode.py
@@ -0,0 +1,10 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import QtGui
+
+if __main__:
+  application = QApplication(sys.argv)
+  window = QWidget()
+  Wİndow.setWindowTitle("Record EEG and Display")
+  Window.show()
+  sys.exit(application.exec())
